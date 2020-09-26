bf964a09de7fac319052924120adfacb6e6f66ce
Nayankumar Dugad <dugadnayan0504@gmail.com>
2020-09-26 15:47:57 +0530

    first commit

---
+++ sumofdiagonal.py
@@ -0,0 +1,26 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+#def diagonalDifference(arr):
+
+if __name__ == '__main__':
+    
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+   
+    a = sum(arr[i][i]  for i in range(n) ) 
+    print(str(a))
+
+
+
+    
