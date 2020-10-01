8c95b74f4cd38218aaca845f9dcfa6e4f274eb32
jainanuj261 <jainanuj261@gmail.com>
2020-10-01 10:30:37 +0530

    changed

--- sumofdiagonal.py
+++ sumofdiagonal.py
@@ -5,11 +5,10 @@ import re
 import sys
 
 
-#def diagonalDifference(arr):
+# def diagonalDifference(arr):
 
 if __name__ == '__main__':
-    
-    
+
     n = int(input("Please Enter Number:").strip())
 
     arr = []
@@ -17,11 +16,6 @@ if __name__ == '__main__':
     for _ in range(n):
         arr.append(list(map(int, input().rstrip().split())))
 
-   
-    a = sum(arr[i][i]  for i in range(n) ) 
+    a = sum(arr[i][i] for i in range(n))
     print(str(a))
     # Hello Nayan Dugad
-
-
-
-    
